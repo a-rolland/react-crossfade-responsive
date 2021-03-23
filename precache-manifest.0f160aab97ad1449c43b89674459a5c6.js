e8a80bddf1c3f615f0e303ba3e7c6f80d806800a
a-rolland <antoine-rolland@hotmail.fr>
2021-03-23 10:13:21 +0100

    Updates

---
+++ precache-manifest.0f160aab97ad1449c43b89674459a5c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdbb3d66845ab50caf7b304602c8a1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67b945ad0367731b934c",
+    "url": "./static/css/main.c9a14258.chunk.css"
+  },
+  {
+    "revision": "66f929e4a0fcf136044e",
+    "url": "./static/js/2.205c747e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.205c747e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b945ad0367731b934c",
+    "url": "./static/js/main.3790e7cf.chunk.js"
+  },
+  {
+    "revision": "74a130a58941af7c84f2",
+    "url": "./static/js/runtime-main.6edd6be9.js"
+  }
+]);
